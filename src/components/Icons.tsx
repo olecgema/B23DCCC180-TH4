7179f8529c6ef08bd159b9ebfa25a5f206d29523
lehoang281105 <lehoang281105@gmail.com>
2025-03-13 20:30:07 +0700

    lehoang281105

---
+++ src/components/Icons.tsx
@@ -0,0 +1,11 @@
+import {
+	UserOutlined,
+	ToolOutlined,
+	CalendarOutlined,
+	StarOutlined,
+	DollarOutlined,
+	HomeOutlined,
+	ArrowsAltOutlined,
+} from '@ant-design/icons';
+
+export { UserOutlined, ToolOutlined, CalendarOutlined, StarOutlined, DollarOutlined, HomeOutlined, ArrowsAltOutlined };
